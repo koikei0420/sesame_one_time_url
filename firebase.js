4d7d11eee266d135901edd92325e97728ec9f101
koikei0420 <koikei0420@gmail.com>
2020-03-29 00:46:08 +0900

    initial commit

---
+++ firebase.js
@@ -0,0 +1,12 @@
+/* vim:ts=4:sw=4:sts=4:st:et */
+
+export var firebaseConfig = {
+    apiKey: "AIzaSyCmLQ74MkQo6PcIzjXXPCYZoTjEdfYnPtw",
+    authDomain: "keisuke-lock.firebaseapp.com",
+    databaseURL: "https://keisuke-lock.firebaseio.com",
+    projectId: "keisuke-lock",
+    storageBucket: "keisuke-lock.appspot.com",
+    messagingSenderId: "598331700377",
+    appId: "1:598331700377:web:953ccab37ca584fb57b408",
+    measurementId: "G-WX58D8S8M3",
+};
