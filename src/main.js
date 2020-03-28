abb7f0429724d7e4f528d8355a924cf0f07775e1
koikei0420 <koikei0420@gmail.com>
2020-03-29 00:43:07 +0900

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
 import store from './store'
+import vuetify from './plugins/vuetify';
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import firebaseConfig from '../credentials/firebase.js';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+      router,
+      store,
+      vuetify,
+      render: h => h(App)
 }).$mount('#app')
---
+++ src/router/index2.js
@@ -0,0 +1,33 @@
+/* vim:ts=2:sw=2:sts=2:st:et */
+
+import Vue from 'vue';
+import Router from 'vue-router';
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Signin from '@/components/Signin'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
