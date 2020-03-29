07161ad5f3b16d2886a5c71df965f48e74e30bdd
koikei0420 <koikei0420@gmail.com>
2020-03-29 16:08:36 +0900

    comment out store

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
-import store from './store'
+//import store from './store'
 import vuetify from './plugins/vuetify';
 import firebase from 'firebase/app'
 import 'firebase/auth'
@@ -14,7 +14,7 @@ firebase.initializeApp(firebaseConfig);
 
 new Vue({
       router,
-      store,
+//      store,
       vuetify,
       render: h => h(App)
 }).$mount('#app')
